1206b43d3b4daa2667e00a82a9b13007451f0e6b
Hendrikus  van Katwijk <hendrikus.hpvk@gmail.com>
2022-05-19 15:22:41 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Date Malta</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -19,35 +19,28 @@
             height: 100vh;
             margin: 0;
         }
-
         .full-height {
             height: 100vh;
         }
-
         .flex-center {
             align-items: center;
             display: flex;
             justify-content: center;
         }
-
         .position-ref {
             position: relative;
         }
-
         .top-right {
             position: absolute;
             right: 10px;
             top: 18px;
         }
-
         .content {
             text-align: center;
         }
-
         .title {
             font-size: 84px;
         }
-
         .links > a {
             color: #636b6f;
             padding: 0 25px;
@@ -57,7 +50,6 @@
             text-decoration: none;
             text-transform: uppercase;
         }
-
         .m-b-md {
             margin-bottom: 30px;
         }
@@ -69,7 +61,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Dating App
+            Date Malta App
         </div>
         <div class="links">
             @if (Route::has('login'))
@@ -83,6 +75,15 @@
                     @endif
                 @endauth
             @endif
+            
+            <p class="fh5co-copyright">
+			    <small>
+                    (c) 2015 <a href="index.html">Vanniks</a>. All Rights Reserved. <br>
+                    Designed by: <a href="http://hpvk.com/">hpvk.com</a> Images: 
+                    <a href="http://plmd.me/" target="_blank">plmd.me</a> &amp; 
+                    <a href="http://unsplash.com/" target="_blank">Unsplash</a> 
+                </small>
+			</p>
         </div>
 
     </div>
