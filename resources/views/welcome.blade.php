f3a2a25d09be7b22a2ca639339d8ee87d4bef4a2
Hendrikus  van Katwijk <hendrikus.hpvk@gmail.com>
2022-05-19 16:06:47 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,46 +80,65 @@
         }
         
         
-        
-        
-.nicebtn{
-  display: block;
-  width: 200px;
-  height: 40px;
-  line-height: 40px;
-  font-size: 18px;
-  font-family: sans-serif;
-  text-decoration: none;
-  color: #333;
-  border: 2px solid #333;
-  letter-spacing: 2px;
-  text-align: center;
-  position: relative;
-  transition: all .35s;
+
+.glow-on-hover {
+    width: 220px;
+    height: 50px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
 }
 
-.nicebtn span{
-  position: relative;
-  z-index: 2;
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
 }
 
-.nicebtn:after{
-  position: absolute;
-  content: "";
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 100%;
-  background: #ff003b;
-  transition: all .35s;
+.glow-on-hover:active {
+    color: #000
 }
 
-.nicebtn:hover{
-  color: #fff;
+.glow-on-hover:active:after {
+    background: transparent;
 }
 
-.nicebtn:hover:after{
-  width: 100%;
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
 }
     </style>
 </head>
@@ -135,12 +154,12 @@
         <div class="links">
             @if (Route::has('login'))
                 @auth
-                    <a class="nicebtn" href="{{ url('/home') }}">Home</a>
+                    <a class="glow-on-hover" href="{{ url('/home') }}">Home</a>
                 @else
-                    <a class="nicebtn" href="{{ route('login') }}">Login</a>
+                    <a class="glow-on-hover" href="{{ route('login') }}">Login</a>
 
                     @if (Route::has('register'))
-                        <a class="nicebtn" href="{{ route('register') }}">Register</a>
+                        <a class="glow-on-hover" href="{{ route('register') }}">Register</a>
                     @endif
                 @endauth
             @endif
